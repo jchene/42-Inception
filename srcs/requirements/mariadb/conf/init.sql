4c69f3b01e78541eca04571514b8e94f3b9fa41c
jchene <jchene@student.42.fr>
2023-02-17 18:52:30 +0100

    finished?

--- srcs/requirements/mariadb/conf/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'mysql'@'localhost' IDENTIFIED BY 'mysql';
-GRANT ALL PRIVILEGES ON inception.* TO 'mysql'@'localhost' IDENTIFIED BY 'mysql';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'toor';
-use mysql;
-repair table user;
-repair table db;
